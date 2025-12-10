159c520a615ff9aa22f39a5c4938a49f1e45277e
Sonu Kumar Napit <sonu714169@gmail.com>
2025-12-10 13:40:35 +0530

    Update FacialExpression.jsx

--- Frontend/src/components/FacialExpression.jsx
+++ Frontend/src/components/FacialExpression.jsx
@@ -39,7 +39,7 @@ export default function FacialExpression({ setSongs }) {
             }
         }
 
-        axios.get('https://moody-player1-1-backend.onrender.com')
+        axios.get(`https://moody-player1-1-backend.onrender.com/songs?mood=${_expression}`)
             .then(response => {
                 console.log(response.data);
                 setSongs(response.data.songs);
